1a2002cde1380eb4927b73c4a4eeacc5934a3bb7
charith <chariththarindu@gmail.com>
2020-01-03 22:46:43 +0530

    initial commit

---
+++ src/test/java/com/saviya/api/config/server/SaviyaAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saviya.api.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaviyaAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
